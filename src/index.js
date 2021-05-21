7b6a7db500c1a5cdf48faba519eb7be8e47849d7
zeus <zeus.ramirez347@gmail.com>
2021-05-21 16:06:20 -0400

    remove task button

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault()
    let taskInput = e.target[0].value
    let listElly = document.createElement('li');
+   let delBtn = document.createElement('button');
+   delBtn.textContent = ('x')
   listElly.textContent = taskInput;
   document.querySelector('ul').append(listElly)
+  listElly.append(delBtn)
+  delBtn.addEventListener('click', (e)=>{
+    listElly.remove()
+
   })
+  })
+  
 })
